866a6c77cdc6c9521e24e1c69a16a74bee847740
trentstanley92 <trentstanley92@gmail.com>
2023-03-30 21:04:49 -0400

    adds updated java script code

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,19 @@
 // Assignment Code
+function generatePassword(){
+// Creates window to ask for password length
+var length = window.prompt("choose a password length between 8 and 128 characters")
+// available characters
+char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOIP0123456789!@#$%^&*()"
+
+
+// for loop to generate password length
+passwordText =[""]
+  for(i=0;i < length; i++){
+    passwordText += char.charAt(Math.random() * char.length)
+    return passwordText
+  }
+
+};
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,6 +24,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
